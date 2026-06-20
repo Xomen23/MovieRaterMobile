cc2bc7116c893b37300afc235d1a0bffe83e48f6
petar-mitrovic <pm20220179@student.fon.bg.ac.rs>
2026-06-20 14:50:09 +0200

    add JPA entities for user, movie and review mapping

---
+++ movie-backend/src/main/java/com/example/movie_rater/entity/MovieEntity.java
@@ -0,0 +1,30 @@
+package com.example.movie_rater.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "movie")
+public class MovieEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "imdb_id", unique = true)
+    private String imdbId;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "movie_year")
+    private Integer year;
+}
---
+++ movie-backend/src/main/java/com/example/movie_rater/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package com.example.movie_rater.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "\"user\"")
+public class UserEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
